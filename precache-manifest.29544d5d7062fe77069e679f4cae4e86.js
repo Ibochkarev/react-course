8c65f64efd9c3047eb24288ab60b567420bb2205
Ibochkarev <ivanx86@gmail.com>
2020-02-13 12:18:41 +0600

    Updates

---
+++ precache-manifest.29544d5d7062fe77069e679f4cae4e86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50712a631c3e0f2f66e9a62af06ce72c",
+    "url": "/react-course/index.html"
+  },
+  {
+    "revision": "7159a376ab2398b3f612",
+    "url": "/react-course/static/css/main.c3394f1b.chunk.css"
+  },
+  {
+    "revision": "5972ab9a24d87de2b217",
+    "url": "/react-course/static/js/2.40aee04e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-course/static/js/2.40aee04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7159a376ab2398b3f612",
+    "url": "/react-course/static/js/main.7f9a8393.chunk.js"
+  },
+  {
+    "revision": "31a8dcc2c9fba20482b2",
+    "url": "/react-course/static/js/runtime-main.b56428b5.js"
+  }
+]);
